14792afbb0b5c78a1f757d3b5f40b0e881492c9c
claudiopatti <claudiopatti1997@gmail.com>
2024-10-24 19:02:06 +0200

    aggiunto lang ma non riuscito a cambiare in it e aggiunto old per gli input per visuallizare cosa si è scritto prima dell'errore

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -23,7 +23,7 @@
 
     <div class="mb-3">
         <label for="title" class="form-label">Title <span class="text-danger">*</span></label>
-        <input type="text" class="form-control" name="title" id="title" placeholder="Inserisci il titolo del fumetto....." required maxlength="64">
+        <input type="text" class="form-control" name="title" id="title" placeholder="Inserisci il titolo del fumetto....." required maxlength="64" value="{{ old('title') }}">
         @error('title')
             <div class="alert alert-danger my-4">
                 ERRORE TITLE: {{ $message }}
@@ -32,7 +32,7 @@
     </div>
     <div class="mb-3">
         <label for="description" class="form-label">Description<span class="text-danger">*</span></label>
-        <textarea type="text" class="form-control" name="description" id="description" placeholder="Inserisci la descrizone del fumetto....." maxlength="4096" required></textarea>
+        <textarea type="text" class="form-control" name="description" id="description" placeholder="Inserisci la descrizone del fumetto....." maxlength="4096" required>{{ old('description') }}"</textarea>
         @error('description')
             <div class="alert alert-danger my-4">
                 ERRORE DESCRIZIONE: {{ $message }}
@@ -41,7 +41,7 @@
     </div>
     <div class="mb-3">
         <label for="thumb" class="form-label">Thumb</label>
-        <input type="text" class="form-control" name="thumb" id="thumb" placeholder="Inserisci la foto del fumetto.....">
+        <input type="text" class="form-control" name="thumb" id="thumb" placeholder="Inserisci la foto del fumetto....." value="{{ old('thumb') }}">
         @error('thumb')
             <div class="alert alert-danger my-4">
                 ERRORE THUMB: {{ $message }}
@@ -50,7 +50,7 @@
     </div>
     <div class="mb-3">
         <label for="price" class="form-label">Price<span class="text-danger">*</span></label>
-        <input type="number" class="form-control" name="price" id="price" placeholder="Inserisci il prezzo (con punto se ci sono dei centesimi) del fumetto....." required min="0" step=".01">
+        <input type="number" class="form-control" name="price" id="price" placeholder="Inserisci il prezzo (con punto se ci sono dei centesimi) del fumetto....." required min="0" step=".01"  value="{{ old('price') }}">
         @error('price')
             <div class="alert alert-danger my-4">
                 ERRORE PRICE: {{ $message }}
@@ -59,7 +59,7 @@
     </div>
     <div class="mb-3">
         <label for="series" class="form-label">Series<span class="text-danger">*</span></label>
-        <input type="text" class="form-control" name="series" id="series" placeholder="Inserisci la serie del fumetto....." required maxlength="64">
+        <input type="text" class="form-control" name="series" id="series" placeholder="Inserisci la serie del fumetto....." required maxlength="64" value="{{ old('series') }}">
         @error('series')
             <div class="alert alert-danger my-4">
                 ERRORE SERIES: {{ $message }}
@@ -68,7 +68,7 @@
     </div>
     <div class="mb-3">
         <label for="sale_date" class="form-label">Sale_date</label>
-        <input type="date" class="form-control" name="sale_date" id="sale_date" placeholder="Inserisci la data di uscita del fumetto.....">
+        <input type="date" class="form-control" name="sale_date" id="sale_date" placeholder="Inserisci la data di uscita del fumetto....." value="{{ old('sale_date') }}">
         @error('sale_date')
             <div class="alert alert-danger my-4">
                 ERRORE SALE_DATE: {{ $message }}
@@ -78,9 +78,21 @@
     <div class="mb-3">
         <label for="type" class="form-label">Type<span class="text-danger">*</span></label>
         <select class="form-select" aria-label="Default select example" id="type" name="type" required>
-            <option selected disabled>inserisci il tipo del fumetto</option>
-            <option value="comic book">Comic book</option>
-            <option value="graphic novel">Graphic novel</option>
+            <option
+                @if (!isset(old('type')) || old('type') == '')
+                    selected
+                @endif 
+                disabled>inserisci il tipo del fumetto</option>
+            <option
+                @if (old('type') == 'comic book')
+                    selected
+                @endif  
+                value="comic book">Comic book</option>
+            <option 
+                @if (old('type') == 'graphic novel')
+                    selected
+                @endif 
+                value="graphic novel">Graphic novel</option>
           </select>
           @error('type')
             <div class="alert alert-danger my-4">
@@ -90,7 +102,7 @@
     </div>
     <div class="mb-3">
         <label for="artists" class="form-label">Artists</label>
-        <input type="text" class="form-control" name="artists" id="artists" aria-describedby="artists-help" placeholder="Inserisci Gli artisti del fumetto.....">
+        <input type="text" class="form-control" name="artists" id="artists" aria-describedby="artists-help" placeholder="Inserisci Gli artisti del fumetto....." value="{{ old('artists') }}">
         <div id="artists-help" class="form-text">
             Inserisci i nomi degli artisti divisi da virgole
         </div>
@@ -102,7 +114,7 @@
     </div>
     <div class="mb-3">
         <label for="writers" class="form-label">Writers</label>
-        <input type="text" class="form-control" name="writers" id="writers" aria-describedby="writers-help" placeholder="Inserisci gli scrittori del fumetto.....">
+        <input type="text" class="form-control" name="writers" id="writers" aria-describedby="writers-help" placeholder="Inserisci gli scrittori del fumetto....." value="{{ old('writers') }}">
         <div id="writers-help" class="form-text">
             Inserisci i nomi degli scrittori divisi da virgole
         </div>
